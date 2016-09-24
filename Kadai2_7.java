5d8e486441bca4c80d6946377a60d9ba347a85a0
SHOUEI\翔栄 <dxbjr492@yahoo.co.jp>
2016-09-25 00:43:53 +0900

    入力できる値を0～50にした。

--- Kadai2_7.java
+++ Kadai2_7.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Kadai2_7
 {
 	public static void main(String[] args) {
-		System.out.println("整数を入力してください。");
+		System.out.println("0～50までの整数を選んで入力してください。");
 		Scanner scan = new Scanner(System.in);
 		int intSeisu = scan.nextInt();
-		System.out.println(fibo(intSeisu));
+		if(intSeisu > 50){
+			System.out.println("入力値が0～50ではなかったので終了します。");
+			System.exit(0);
+		}
+		for(int i = 0;i <= intSeisu; i++){
+			System.out.println(fibo(i));
+		}
 	}
 
 	public static long fibo(int n) {
